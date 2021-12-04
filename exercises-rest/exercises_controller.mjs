b16d43b2315ceaa9726204088c4353addd034063
Jonathon Shea <sheajonathon0@gmail.com>
2021-12-04 15:19:36 -0500

    Created controller and model for exercises

---
+++ exercises-rest/exercises_controller.mjs
@@ -0,0 +1,51 @@
+import * as exercises from './exercises_model.mjs';
+import express from 'express';
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.json());
+
+/**
+ * Create a new movie with the title, year and language provided in the body
+ */
+app.post('/exercises', (req, res) => {
+    
+});
+
+
+/**
+ * Retrive the movie corresponding to the ID provided in the URL.
+ */
+app.get('/exercises/:_id', (req, res) => {
+    
+});
+
+/**
+ * Retrieve exercises. 
+ * If the query parameters include a year, then only the exercises for that year are returned.
+ * Otherwise, all exercises are returned.
+ */
+app.get('/exercises', (req, res) => {
+  
+});
+
+/**
+ * Update the movie whose id is provided in the path parameter and set
+ * its title, year and language to the values provided in the body.
+ */
+app.put('/exercises/:_id', (req, res) => {
+    
+});
+
+/**
+ * Delete the movie whose id is provided in the query parameters
+ */
+app.delete('/exercises/:_id', (req, res) => {
+    
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
---
+++ exercises-rest/exercises_model.mjs
@@ -0,0 +1,32 @@
+// Get the mongoose object
+import mongoose from 'mongoose';
+
+// Prepare to the database exercises_db in the MongoDB server running locally on port 27017
+mongoose.connect(
+    "mongodb://localhost:27017/exercises_db",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+// Connect to to the database
+const db = mongoose.connection;
+// The open event is called when the database connection successfully opens
+db.once("open", () => {
+    console.log("Successfully connected to MongoDB using Mongoose!");
+});
+
+/**
+ * Define the schema
+ */
+const exerciseSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    reps: { type: Number, required: true },
+    weight: {type: Number, required: true},
+    unit: {type: String, required: true},
+    date: {type: String, required: true}
+});
+
+/**
+ * Compile the model from the schema. This must be done after defining the schema.
+ */
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
